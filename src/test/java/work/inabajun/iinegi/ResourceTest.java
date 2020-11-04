448a0824b3e6fe297acacfdd72712cef17231307
inabajunmr <inabajun.for.regi@gmail.com>
2020-11-04 23:22:23 +0900

    init QUARKUS

---
+++ src/test/java/work/inabajun/iinegi/ResourceTest.java
@@ -0,0 +1,21 @@
+package work.inabajun.iinegi;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class ResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/negis")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}

24230207802232a2e5bc0cee97b5bae7dd78a825
inabajunmr <inabajun.for.regi@gmail.com>
2020-11-16 00:11:14 +0900

    wip

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (main)
 
 import Browser
+import File
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -25,6 +26,7 @@ main =
 type alias Model =
     { input : String
     , negiState : NegiState
+    , files : List File.File
     }
 
 
@@ -37,10 +39,10 @@ type NegiState
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model "" Init
+    ( Model "" Init []
     , Http.get
         { url = "http://0.0.0.0:8080/negi?after=1605367129000" -- 15 Nov 2020 17:19:15
-        , expect = Http.expectJson Receive negisDecoder
+        , expect = Http.expectJson Received negisDecoder
         }
     )
 
@@ -50,27 +52,70 @@ init _ =
 
 
 type Msg
-    = Receive (Result Http.Error (List Negi))
+    = Received (Result Http.Error (List Negi))
+    | SelectImage (List File.File)
+    | Upload
+    | Uploaded (Result Http.Error ())
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Receive (Ok user) ->
-            ( { model | negiState = Loaded user }, Cmd.none )
+        Received (Ok negi) ->
+            ( { model | negiState = Loaded negi }, Cmd.none )
 
-        Receive (Err e) ->
+        Received (Err e) ->
             ( { model | negiState = Failed e }, Cmd.none )
+
+        -- TODO
+        Uploaded (Ok ok) ->
+            ( model, Cmd.none )
+
+        Uploaded (Err e) ->
+            ( model, Cmd.none )
+
+        SelectImage files ->
+            ( { model | files = files }, Cmd.none )
+
+        Upload ->
+            ( model, upload model.files )
+
+
+
+-- TODO
+-- TODO https://package.elm-lang.org/packages/elm/http/latest/Http#request
+
+
+upload : List File.File -> Cmd Msg
+upload files =
+    case List.head files of
+        Just file ->
+            Http.request
+                { method = "POST"
+                , headers = []
+                , url = "http://0.0.0.0:8080/negi"
+                , body =
+                    Http.multipartBody
+                        [ Http.stringPart "description" "Test", Http.filePart "image" file ]
+                , expect = Http.expectWhatever Uploaded
+                , timeout = Nothing
+                , tracker = Nothing
+                }
+
+        Nothing ->
+            Cmd.none
 
 
 
 -- VIEW
+-- https://elm-lang.org/examples/upload
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ case model.negiState of
+        [ form model
+        , case model.negiState of
             Init ->
                 text ""
 
@@ -86,7 +131,26 @@ view model =
 
             Failed e ->
                 div [] [ text (Debug.toString e) ]
+        , text (Debug.toString model.files)
         ]
+
+
+form : Model -> Html Msg
+form model =
+    div []
+        [ input
+            [ type_ "file"
+            , multiple True
+            , on "change" (D.map SelectImage filesDecoder)
+            ]
+            []
+        , button [ onClick Upload ] [ text "Submit" ]
+        ]
+
+
+filesDecoder : D.Decoder (List File.File)
+filesDecoder =
+    D.at [ "target", "files" ] (D.list File.decoder)
 
 
 
